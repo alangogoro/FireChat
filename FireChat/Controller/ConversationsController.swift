d09d18aca1bc3c4a4375099e973a8f6bdfc5ae63
alangogoro <cayne9160@gmail.com>
2020-12-06 16:13:41 +0800

    NavigationBar Setup

--- FireChat/Controller/ConversationsController.swift
+++ FireChat/Controller/ConversationsController.swift
@@ -22,13 +22,34 @@ class ConversationsController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         
-        // NavigationBar 標題文字放大
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.title = "Messages"
+        configureNavigationBar()
         // 設定 NavigationItem - LeftBarButtonItem
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain,
                                                            target: self, action: #selector(showProfile))
+    }
+    
+    func configureNavigationBar() {
+        /* 建立 NavigationBarAppearance
+         * 調整標題顏色及紫色不透明背景 */
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = .systemPurple
+        /* ⚠️ 將 NavigationBarAppearance 套用在 NavigationBar 上 */
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        // NavigationBar 標題文字樣式
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        //                                  translucent 半透明的
+        /* ‼️ 切換使用者導覽列（時鐘、電池）的樣式 ⚠️
+         * 分為淺色與深色*/
+        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
     }
     
     // MARK: - Selectors
