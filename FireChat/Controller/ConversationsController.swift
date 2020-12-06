d95dcb7aef8a5f1d0fd732aecafdb9b762afa0fd
alangogoro <cayne9160@gmail.com>
2020-12-06 14:09:25 +0800

    init commit

---
+++ FireChat/Controller/ConversationsController.swift
@@ -0,0 +1,23 @@
+//
+//  ConversationsController.swift
+//  FireChat
+//
+//  Created by usr on 2020/12/6.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Helpers
+    
+}
--- FireChat/SceneDelegate.swift
+++ FireChat/SceneDelegate.swift
@@ -13,10 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        /* 設置 Window 的 Scene */
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        /* 生成並設定 NavigationController 的 Root */
+        let navControl = UINavigationController(rootViewController: ConversationsController())
+        window?.rootViewController = navControl
+        //
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
