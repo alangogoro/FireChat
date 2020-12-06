8f6bbe9e75913929761ec5e8254b36ea34482317
alangogoro <cayne9160@gmail.com>
2020-12-06 18:15:15 +0800

    04. Login Screen

---
+++ FireChat/Controller/Authentication/LoginController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginController.swift
+//  FireChat
+//
+//  Created by usr on 2020/12/6.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+        
+    }
+    
+    // MARK: - Helpers
+    
+}
---
+++ FireChat/Controller/Authentication/ResgistrationController.swift
@@ -0,0 +1,24 @@
+//
+//  ResgistrationController.swift
+//  FireChat
+//
+//  Created by usr on 2020/12/6.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+        
+    }
+    
+    // MARK: - Helpers
+    
+}
