5316c5040451fa255512c426362b371a700360c9
alangogoro <cayne9160@gmail.com>
2020-12-07 17:55:43 +0800

    Added StackView & UIs

--- FireChat/Controller/Authentication/LoginController.swift
+++ FireChat/Controller/Authentication/LoginController.swift
@@ -19,6 +19,31 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private let emailContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        view.setHeight(height: 50)
+        return view
+    }()
+    private let passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        view.setHeight(height: 50)
+        return view
+    }()
+    
+    private let loginButton: UIButton = {
+        /* 建立並設定 UIButton 按鈕 */
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.backgroundColor  = .systemRed
+        button.setHeight(height: 50)
+        return button
+    }()
+    
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +61,12 @@ class LoginController: UIViewController {
         configureGradientLayer()
         
         view.addSubview(iconImageView)
+        /* ===== 使用了 UIView 的 Autokayout extension ===== */
+        iconImageView.centerX(inView: view)
+        iconImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                             paddingTop: 32)
+        iconImageView.setDimensions(height: 120, width: 120 )
+        /* ===== 未使用 UIView 的 Autokayout extension 時，必須這樣寫 =====
         /* ‼️ 啟用程式碼所寫的 AutoLayout ⚠️
          * ⛔️ 如果不加這一行，UI 不會顯示！ */
         iconImageView.translatesAutoresizingMaskIntoConstraints = false
@@ -44,6 +75,19 @@ class LoginController: UIViewController {
         iconImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         iconImageView.heightAnchor.constraint(equalToConstant: 120).isActive = true
         iconImageView.widthAnchor.constraint(equalToConstant: 120).isActive = true
+         */
+        
+        /* 建立 StackView */
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   loginButton])
+        stack.axis = .vertical
+        stack.spacing = 16
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImageView.bottomAnchor,
+                     left: view.leftAnchor, right: view.rightAnchor,
+                     paddingTop: 32, paddingLeft: 32, paddingRight: 32)
     }
     /// 設置漸層背景
     func configureGradientLayer() {
