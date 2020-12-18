e5b95c57acf4edccd07ba0559c9f918afa877a3b
alangogoro <cayne9160@gmail.com>
2020-12-18 18:34:30 +0800

    Finished UserCell fetch & configure plus bug fixed!

--- FireChat/Controller/NewMessageController.swift
+++ FireChat/Controller/NewMessageController.swift
@@ -13,17 +13,28 @@ private let reuseIdentifier = "UserCell"
 class NewMessageController: UITableViewController {
     
     // MARK: - Properties
+    private var users = [User]()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        fetchUsers()
+        
     }
     
     // MARK: - Selector
     @objc func handleDismissal() {
         dismiss(animated: true)
+    }
+    
+    // MARK: - API
+    func fetchUsers() {
+        Service.fetchUsers { users in
+            self.users = users
+            self.tableView.reloadData()
+        }
     }
     
     // MARK: - Helpers
@@ -36,7 +47,7 @@ class NewMessageController: UITableViewController {
         /* 用 FooterView 取代空白處（去掉 Cell 之間的分隔線） */
         tableView.tableFooterView = UIView()
         /* 註冊 TableVIew 的 Cell class 以及 reuseIdentifier */
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 80
     }
 }
@@ -44,11 +55,13 @@ class NewMessageController: UITableViewController {
 // MARK: - UITableViewDataSource
 extension NewMessageController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return users.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-        cell.textLabel?.text = "User Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier,
+                                                 for: indexPath)
+            as! UserCell
+        cell.user = users[indexPath.row]
         return cell
     }
 }
--- FireChat/View/UserCell.swift
+++ FireChat/View/UserCell.swift
@@ -6,10 +6,16 @@
 //
 
 import UIKit
+import SDWebImage
 
 class UserCell: UITableViewCell {
     
     // MARK: - Properties
+    
+    var user: User? {
+        didSet { configure() }
+    }
+    
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.backgroundColor = .systemPurple
@@ -60,4 +66,13 @@ class UserCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Helpers
+    func configure() {
+        guard let user = user else { return }
+        fullnameLabel.text = user.fullname
+        usernameLabel.text = user.username
+        
+        guard let url = URL(string: user.profileImageUrl) else { return }
+        profileImageView.sd_setImage(with: url)
+    }
 }
